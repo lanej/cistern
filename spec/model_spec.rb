74e88ffb14062220fb9808cfe34ca4ade406bdc8
Josh Lane <me@joshualane.com>
2012-08-29 21:53:37 -0700

    loosen object id hex matching

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -12,14 +12,14 @@ describe "Cistern::Model" do
     end
 
     it "should use default" do
-      Sample.new(id: 1, name: "name").inspect.should match /#<Sample:0x[0-9a-f]{12} attributes={id:1,name:\"name\"}/
+      Sample.new(id: 1, name: "name").inspect.should match /#<Sample:0x[0-9a-f]+ attributes={id:1,name:\"name\"}/
     end
 
     it "should use awesome_print" do
       defined?(AwesomePrint).should be_false # don't require if not used
       Sample.formatter= Cistern::Formatter::AwesomePrint
 
-      Sample.new(id: 1, name: "name").inspect.match /(?x-mi:\#<Sample:0x[0-9a-f]{12}>\ {\n\ \ \ \ \ \ :id\x1B\[0;37m\ =>\ \x1B\[0m\x1B\[1;34m1\x1B\[0m,\n\ \ \ \ :name\x1B\[0;37m\ =>\ \x1B\[0m\x1B\[0;33m"name"\x1B\[0m\n})/
+      Sample.new(id: 1, name: "name").inspect.match /(?x-mi:\#<Sample:0x[0-9a-f]+>\ {\n\ \ \ \ \ \ :id\x1B\[0;37m\ =>\ \x1B\[0m\x1B\[1;34m1\x1B\[0m,\n\ \ \ \ :name\x1B\[0;37m\ =>\ \x1B\[0m\x1B\[0;33m"name"\x1B\[0m\n})/
     end
 
     it "should use formatador" do
