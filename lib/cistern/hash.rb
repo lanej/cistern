ef45639764bc712b6e07c8dba26d2deedbe6db1a
Thom Mahoney <tmahoney@engineyard.com>
2014-06-13 10:50:56 -0700

    add Cistern::Hash#stringify_keys
    also adds specs for Cistern::Hash

--- lib/cistern/hash.rb
+++ lib/cistern/hash.rb
@@ -1,7 +1,18 @@
 class Cistern::Hash
   def self.slice(hash, *keys)
     {}.tap do |sliced|
-      keys.each{|k| sliced[k]= hash[k] if hash.key?(k)}
+      keys.each{ |k| sliced[k] = hash[k] if hash.key?(k) }
+    end
+  end
+
+  def self.stringify_keys(object)
+    case object
+    when Hash
+      object.inject({}){|r,(k,v)| r.merge(k.to_s => stringify_keys(v))}
+    when Array
+      object.map{|v| stringify_keys(v) }
+    else
+      object
     end
   end
 end
---
+++ spec/cistern_hash_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "cistern hashes" do
+  describe "#slice" do
+    let(:input) do
+      { one: "one", two: "two", three: "three" }
+    end
+
+    it "returns a new hash with only the specified keys" do
+      expect(Cistern::Hash.slice(input, :one, :two)).to eq({ one: "one", two: "two" })
+    end
+
+    it "skips keys that aren't in the original hash" do
+      expect(Cistern::Hash.slice(input, :four)).to eq({})
+    end
+  end
+
+  describe "#stringify_keys" do
+    let(:input) do
+      { one: "one", two: "two" }
+    end
+
+    it "returns a new hash with stringed keys" do
+      expect(Cistern::Hash.stringify_keys(input)).to eq({ "one" => "one", "two" => "two" })
+    end
+
+    context "with nested hashes or arrays" do
+      let(:input) do
+        { hash: { one: "one" }, array: [{ two: "two" }] }
+      end
+
+      it "stringifies all of the keys" do
+        expect(Cistern::Hash.stringify_keys(input)).to eq({ "hash" => { "one" => "one" }, "array" => [{ "two" => "two" }]})
+      end
+    end
+  end
+end
