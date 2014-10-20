7301c995e95bb08de82a1796e288522a23b3f2c4
Josh Lane <me@joshualane.com>
2014-10-20 08:48:01 -0700

    underscore

--- lib/cistern/string.rb
+++ lib/cistern/string.rb
@@ -2,4 +2,15 @@ class Cistern::String
   def self.camelize(string)
     string.gsub(/[A-Z]+/) { |w| "_#{w.downcase}" }.gsub(/^_/, "")
   end
+
+  # File activesupport/lib/active_support/inflector/methods.rb, line 90
+  def self.underscore(camel_cased_word)
+    word = camel_cased_word.to_s.gsub('::', '/')
+    word.gsub!(/(?:([A-Za-z\d])|^)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
+    word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
 end
